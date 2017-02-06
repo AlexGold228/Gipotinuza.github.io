c7054c5b28dbb123d2a5f1d86b85510dc256b8a0
Geek <maxmudow.emil123@yandex.ru>
2017-02-06 18:45:43 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+jQuery('document').ready(function();
+	jQuery('button1').on('click', function(){
+	var katet1, katet2
+	katet1=jQuery('#katet1').val();
+	katet1=jQuery('#katet2').val();
+alert("katet1");	
+	});
